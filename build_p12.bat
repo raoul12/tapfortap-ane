a293110276a320d60960f69c69250c71e01ae568
raoul12 <raoul12@gmail.com>
2012-07-07 19:14:41 +0200

    fixed a little mistake

--- build_p12.bat
+++ build_p12.bat
@@ -1,3 +1,2 @@
-set adt_directory=C:\Program Files (x86)\Adobe\Adobe Flash Builder 4.6\sdks\4.6.0 - AIR 3.4 (monocle)\bin
-"%adt_directory%"/adt -certificate -cn fake 2048-RSA fake.p12 1234
+"%FLEX_HOME%/bin/adt" -certificate -cn fake 2048-RSA fake.p12 1234
 pause
