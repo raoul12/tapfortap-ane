4b33f1b5f376b520797bc724afc034ef20afa677
raoul12 <raoul12@gmail.com>
2013-11-19 16:51:04 +0100

    closed this repo

--- build_ane.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set signing_options=-tsa none -storetype pkcs12 -keystore fake.p12 -storepass 1234
-set dest_ANE=tapfortap.ane 
-set extension_XML=library\src\extension.xml 
-set library_SWC=library\bin\tapfortap-ane.swc 
-set SWF_directory=library\bin
-"%FLEX_HOME%/bin/adt" -package %signing_options% -target ane "%dest_ANE%" "%extension_XML%" -swc "%library_SWC%" -platform Android-ARM -C "%SWF_directory%" library.swf -C native tapfortap-ane-android.jar -platform iPhone-ARM -C "%SWF_directory%" library.swf -C native libTapForTapExtension.a
-pause
--- build_p12.bat
+++
@@ -1,2 +0,0 @@
-"%FLEX_HOME%/bin/adt" -certificate -cn fake 2048-RSA fake.p12 1234
-pause
